8414f10b2a4cf59b3c8fb970df87499028ee0f8c
Parkerhiphop <parkerhiphop027@gmail.com>
2023-01-06 19:20:40 +0900

    feat: initialize models, controllers and api endpoints

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Buyer\BuyerController;
+use App\Http\Controllers\Seller\SellerController;
+use App\Http\Controllers\Category\CategoryController;
+use App\Http\Controllers\Product\ProductController;
+use App\Http\Controllers\Transaction\TransactionController;
+use App\Http\Controllers\User\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +20,30 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+/**
+ * Buyers
+ * We don'y want to crud buyer directly.
+ * -> We should do that in User, so we restrict the api route.
+ */
+Route::resource('buyers', BuyerController::class, ['only' => ['index', 'show']]);
+/**
+ * Sellers
+ */
+Route::resource('sellers', SellerController::class, ['only' => ['index', 'show']]);
+/**
+ * Products
+ */
+Route::resource('products', ProductController::class, ['only' => ['index', 'show']]);
+/**
+ * Transactions
+ */
+Route::resource('transactions', TransactionController::class, ['only' => ['index', 'show']]);
+/**
+ * Categories
+ * TODO: Why don't allow create or edit?
+ */
+Route::resource('categories', CategoryController::class, ['except' => ['create', 'edit']]);
+/**
+ * Users
+ */
+Route::resource('users', UserController::class, ['except' => ['create', 'edit']]);
