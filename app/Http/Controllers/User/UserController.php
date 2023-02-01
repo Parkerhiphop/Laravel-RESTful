a5366e2f1ed6e8fc325a6d56fbd526fe89dfc4a7
Parkerhiphop <parkerhiphop027@gmail.com>
2023-02-01 18:40:28 +0800

    feat: implement crud of user

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
@@ -14,7 +16,10 @@ class UserController extends Controller
      */
     public function index()
     {
-        //
+        $users = User::all();
+
+        return response(['data' => $users], 200);
+            // -> header('Content-type', 'application/json');
     }
 
     /**
@@ -35,7 +40,28 @@ class UserController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $rules = [
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:6|confirmed',
+        ];
+
+        $validator = Validator::make($request->all(), $rules);
+    
+        if ($validator->fails()) {
+            return response(['error' => $validator->errors()], 400);
+        }
+    
+        # ->only: to only include the specified fields in the data array
+        $data = $request->only(['name', 'email', 'password']);
+        $data['password'] = bcrypt($request->password);
+        $data['verified'] = User::UNVERIFIED_USER;
+        $data['verification_token'] = User::generateVerificationCode();
+        $data['admin'] = User::REGULAR_USER;
+    
+        $user = User::create($data);
+    
+        return response(['data' => $user], 201);
     }
 
     /**
@@ -46,7 +72,9 @@ class UserController extends Controller
      */
     public function show($id)
     {
-        //
+        $user = User::findOrFail($id);
+
+        return response(['data' => $user], 200);
     }
 
     /**
@@ -69,7 +97,49 @@ class UserController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $user = User::findOrFail($id);
+
+        $rules = [
+            'email' => 'sometimes|required|email|unique:users,email,' . $user->id,
+            'password' => 'sometimes|required|min:6|confirmed',
+            'admin' => 'sometimes|in:' . User::ADMIN_USER . ',' . User::REGULAR_USER,
+        ];
+    
+        $validator = Validator::make($request->all(), $rules);
+    
+        if ($validator->fails()) {
+            return response(['error' => $validator->errors()], 400);
+        }
+    
+        if ($request->has('name')) {
+            $user->name = $request->name;
+        }
+    
+        if ($request->has('email') && $user->email != $request->email) {
+            $user->verified = User::UNVERIFIED_USER;
+            $user->verification_token = User::generateVerificationCode();
+            $user->email = $request->email;
+        }
+    
+        if ($request->has('password')) {
+            $user->password = bcrypt($request->password);
+        }
+    
+        if ($request->has('admin')) {
+            if (!$user->isVerified()) {
+                return response(['error' => 'Only verified users can modify the admin field.'], 409);
+            }
+    
+            $user->admin = $request->admin;
+        }
+    
+        if (!$user->isDirty()) {
+            return response(['error' => 'You need to specify a different value to update.'], 422);
+        }
+    
+        $user->save();
+    
+        return response(['data' => $user], 200);
     }
 
     /**
@@ -80,6 +150,10 @@ class UserController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $user = User::findOrFail($id);
+
+        $user->delete();
+
+        return response(['data' => $user], 200);
     }
 }
